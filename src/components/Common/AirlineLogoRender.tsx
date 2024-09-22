c18068ff41f79bf6bb75492e4086660de54b178c
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-09-22 05:26:19 +0300

    airline img folder fix

--- src/components/Common/AirlineLogoRender.tsx
+++ src/components/Common/AirlineLogoRender.tsx
@@ -2,12 +2,12 @@
 import { useState } from 'react';
 
 const AirlineLogoRender = ({ airlineCode }) => {
-    const [logoPath, setLogoPath] = useState(`src/assets/airlines/${airlineCode.toLowerCase()}.png`);
+    const [logoPath, setLogoPath] = useState(`/assets/airlines/${airlineCode.toLowerCase()}.png`);
 
     const handleError = () => {
         if (airlineCode.toLowerCase() !== 'thy') {
             // Eğer mevcut logo bulunamazsa THY logosuna yönlendir
-            setLogoPath('src/assets/airlines/thy.png');
+            setLogoPath('/assets/airlines/thy.png');
         }
     };
     return (
